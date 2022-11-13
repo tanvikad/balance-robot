42ad9e532eb18e8a09035ce46a98dce6d5795fa9
echen4628 <echen4628@gmail.com>
2022-11-12 19:57:27 -0800

    adding helper function to control motor values and FPGA reset

--- MCU/spi.c
+++ MCU/spi.c
@@ -20,95 +20,16 @@
 
 #define MCK_FREQ 100000
 
-// Test Case from Appendix A.1, B
-char key[16] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
-                0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C};
-
-char plaintext[16] = {0x32, 0x43, 0xF6, 0xA8, 0x88, 0x5A, 0x30, 0x8D,
-                      0x31, 0x31, 0x98, 0xA2, 0xE0, 0x37, 0x07, 0x34};
-
-char ct[16] = {0x39, 0x25, 0x84, 0x1D, 0x02, 0xDC, 0x09, 0xFB,
-               0xDC, 0x11, 0x85, 0x97, 0x19, 0x6A, 0x0B, 0x32};
-
-
-
-/* 
-// Another test case from Appendix C.1
-char key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
-char plaintext[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-                      0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-char ct[16] = {0x69, 0xC4, 0xE0, 0xD8, 0x6A, 0x7B, 0x04, 0x30,
-               0xD8, 0xCD, 0xB7, 0x80, 0x70, 0xB4, 0xC5, 0x5A};
-*/
-
 ////////////////////////////////////////////////
 // Function Prototypes
 ////////////////////////////////////////////////
 
-// void encrypt(char*, char*, char*);
-// void checkAnswer(char*, char*, char*);
-
-void spin_motor(char m1_val, char m2_val) {
-  int i; 
-  digitalWrite(PA5, 1);
- /* for(i = 0; i < 16; i++){
-    spiSendReceive(m1_val[i]);
-  }
-  for(i = 0; i < 16; i++){
-    spiSendReceive(m2_val[i]);
-  }*/
-
-  spiSendReceiveTwoChar(m1_val, m2_val);
-  while(SPI1->SR & SPI_SR_BSY);
-  digitalWrite(PA5, 0);
-}
-
-int binaryToDecimal(int n)
-{
-    int num = n;
-    int dec_value = 0;
- 
-    // Initializing base value to 1, i.e 2^0
-    int base = 1;
- 
-    int temp = num;
-    while (temp) {
-        int last_digit = temp % 10;
-        temp = temp / 10;
- 
-        dec_value += last_digit * base;
-        base = base * 2;
-    }
-    printf("the decimal number is %d\n", dec_value);
-    return dec_value;
-}
-
-
-
-
-int decimalToBinary(int n)
-{
-    // array to store binary number
-    int binaryNum = 0;
-  
-    // counter for binary array
-    int base = 1;
-    int i = 0;
-    while (n > 0) {
-        // storing remainder in binary array
-        binaryNum += (n % 2) * base;
-        n = n / 2;
-        base = base * 10;
-        i++;
-        
-        if(i >= 7) break;
-    }
-    // printing binary array in reverse order
-    printf("the binary number is %d \n", binaryNum);
-    return binaryNum;
-}
-
+void spin_motor(char, char);
+int binaryToDecimal(int);
+int decimalToBinary(int);
+char set_val(int);
+char set_val_helper(bool, int);
+void force_reset();
 
 ////////////////////////////////////////////////
 // Main
@@ -134,6 +55,7 @@ int main(void) {
 
   // Load and done pins
   pinMode(PA5, GPIO_OUTPUT);  // LOAD
+  pinMode(PA6, GPIO_OUTPUT);  // Reset on FPGA
 
   // debugging LEDs
   pinMode(PA9, GPIO_OUTPUT);
@@ -141,19 +63,21 @@ int main(void) {
   digitalWrite(PA9, 0);
   digitalWrite(PA10, 0);
   
-  // hardware accelerated encryption
-  //encrypt(key, plaintext, cyphertext);
-  m1_val = (char) 40 | 0b1000000;
+  // configure motor
+  m1_val = set_val(-40);
   printf("m1 val is %d", m1_val);
-  m2_val = (char) 100;
+  m2_val = set_val(40);
   spin_motor(m1_val, m2_val);
   
 
   for (int j = 0; j < 20; j++)
   {
+    digitalWrite(PA9, j%2);
+    force_reset()
     spin_motor(m1_val, m2_val);
     for (int i = 0; i < 200000; i++) 
       ;
+    force_reset()
     spin_motor(m2_val, m2_val);
     for (int i = 0; i < 200000; i++) 
       ;
@@ -164,34 +88,73 @@ int main(void) {
 // Functions
 ////////////////////////////////////////////////
 
-void set_val(bool forward, int value){
-  return;
+
+void spin_motor(char m1_val, char m2_val) {
+  int i; 
+  digitalWrite(PA5, 1);
+  spiSendReceiveTwoChar(m1_val, m2_val);
+  while(SPI1->SR & SPI_SR_BSY);
+  digitalWrite(PA5, 0);
 }
 
+int binaryToDecimal(int n)
+{
+    int num = n;
+    int dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+ 
+        dec_value += last_digit * base;
+        base = base * 2;
+    }
+    printf("the decimal number is %d\n", dec_value);
+    return dec_value;
+}
 
+int decimalToBinary(int n)
+{
+    // array to store binary number
+    int binaryNum = 0;
+  
+    // counter for binary array
+    int base = 1;
+    int i = 0;
+    while (n > 0) {
+        // storing remainder in binary array
+        binaryNum += (n % 2) * base;
+        n = n / 2;
+        base = base * 10;
+        i++;
+        
+        if(i >= 7) break;
+    }
+    // printing binary array in reverse order
+    printf("the binary number is %d \n", binaryNum);
+    return binaryNum;
+}
 
-// void encrypt(char * key, char * plaintext, char * cyphertext) {
-//   int i;
+char set_val(int value){
+  if (value < 0){
+    return set_val_helper(1, value*-1);
+  }
+  else{
+    return set_val_helper(0, value);
+  }
+}
 
-//   // Write LOAD high
-//   digitalWrite(PA5, 1);
+char set_val_helper(bool reverse, int value){
+  char motor_output = (char) value;
+  motor_output |= reverse << 7;
+  return motor_output;
+}
 
-//   // Send plaintext
-//   for(i = 0; i < 16; i++) {
-//     spiSendReceive(plaintext[i]);
-//   }
-
-//   // Send the key
-//   for(i = 0; i < 16; i++) {
-//     spiSendReceive(key[i]);
-//   }
-
-//   while(SPI1->SR & SPI_SR_BSY); // Confirm all SPI transactions are completed
-//   digitalWrite(PA5, 0); // Write LOAD low
-
-//   // Wait for DONE signal to be asserted by FPGA signifying that the data is ready to be read out.
-//   while(!digitalRead(PA6));
-
-//   for(i = 0; i < 16; i++) {
-//     cyphertext[i] = spiSendReceive(0);  }
-// }
+void force_reset(){
+  digitalWrite(PA6, 1);
+  digitalWrite(PA6, 0);
+}
