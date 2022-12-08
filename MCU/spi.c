5ced361f5833f54d559ca4d61a77cefc64911aea
Tanvika Dasari <tdasari@hmc.edu>
2022-12-08 14:33:47 -0800

    added some function level comments

--- MCU/spi.c
+++ MCU/spi.c
@@ -19,14 +19,16 @@
 ////////////////////////////////////////////////
 // Main
 ////////////////////////////////////////////////
-
 int main(void) {
-
   init();
-  loop();
+  tim_main(TIM6, 20, waiting, after_waiting);
+
 }
 
-float sigmoid_m(float x){
+////////////////////////////////////////////////
+// control_cap: takes in x, the control effort and caps it at our two limit
+////////////////////////////////////////////////
+float control_cap(float x){
   //float scaler = 0.01;
   //return 2*((1.0/(1.0+exp(-1*(x*scaler))))-0.5);
   if(x < -100) return -100;
@@ -34,6 +36,11 @@ float sigmoid_m(float x){
   return x;
 }
 
+
+
+////////////////////////////////////////////////
+// print_float: prints a float with 3 significant figures
+////////////////////////////////////////////////
 void print_float(float f)
 {
   int before_decimal_place = (int)(f);
@@ -42,8 +49,13 @@ void print_float(float f)
 }
 
 
+////////////////////////////////////////////////
+// waiting : function that is called while the timer is counting to ARR
+// data being read is stored in the struct "values"
+////////////////////////////////////////////////
 void waiting(int i, struct imu_values * values)
 {
+  //debug led is switched on and off based on if the timer is continued to count
   if(i % 2 == 0)
     digitalWrite(DEBUG_LED_PIN_1, 1);
   else
@@ -84,9 +96,13 @@ void waiting(int i, struct imu_values * values)
   values->y_rot = rot_y;
 
 
-
 }
 
+
+////////////////////////////////////////////////
+// after_waiting: function that is called after the timer counts to ARR
+// updated data stored in values is used to update the control effor which is sent to controller
+////////////////////////////////////////////////
 void after_waiting(struct imu_values * values, struct controller* c)
 {
 
@@ -124,88 +140,15 @@ void after_waiting(struct imu_values * values, struct controller* c)
   ce = abs(pow(ce, 1.2));
   if(control_less_zero) ce = ce*-1;*/
 
-  int sigmoid_ce = (int) (sigmoid_m(ce));
-  printf("Try to spin motor with %d", sigmoid_ce);
+  int capped_control = (int) (control_cap(ce));
+  printf("Try to spin motor with %d", capped_control);
 
-  char motor_output = set_val(sigmoid_ce);
-
-  //trying bang bang
-  /*if(falling_forward) motor_output = set_val(-100);
-  else motor_output = set_val(100);*/
-
+  char motor_output = set_val(capped_control);
 
   spin_motor(motor_output, motor_output);
 }
 
 
-
-
-void loop(){
-
-  tim_main(TIM6, 20, waiting, after_waiting);
-
-  /*char m1_val;
-  char m2_val;
-  char imu_wai;
-  char imu_yaw_h;
-  char imu_yaw_l;
-  char temp;
-  char z_high;
-  char z_low;
-  char x_high;
-  char x_low;
-  char y_high;
-  char y_low;
-  char rot_z_high;
-  char rot_z_low;
-  char rot_x_high;
-  char rot_x_low;
-  char rot_y_high;
-  char rot_y_low;
-
-  m1_val = set_val(80);
-
-  while(1){
-    spin_motor(m1_val, m1_val);
- 
-    
-     z_high = read_imu((char) OUTZ_H_A);
-     z_low = read_imu((char) OUTZ_L_A);
-     float z = scale_accel(twosComplement_to_int(z_high, z_low));
-     int z_int = (int)(z * 100);
-     printf("IMU z: %d\n", z_int);
-
-    // x_high = read_imu((char) OUTX_H_A);
-    // x_low = read_imu((char) OUTX_L_A);
-    // float x = scale_accel(twosComplement_to_int(x_high, x_low));
-    // int x_int = (int)(x * 100);
-    // printf("IMU x: %d\n", x_int);
-
-    // y_high = read_imu((char) OUTY_H_A);
-    // y_low = read_imu((char) OUTY_L_A);
-    // float y = scale_accel(twosComplement_to_int(y_high, y_low));
-    // int y_int = (int)(y * 100);
-    // printf("IMU y: %d\n", y_int);
-
-    rot_z_high = read_imu((char) OUTZ_H_G);
-    rot_z_low = read_imu((char) OUTZ_L_G);
-    float rot_z = get_angle(twosComplement_to_int(rot_z_high, rot_z_low));
-    int rot_z_int = (int)(rot_z * 100);
-    printf("IMU z angle: %d\n", rot_z_int);
-
-    rot_x_high = read_imu((char) OUTX_H_G);
-    rot_x_low = read_imu((char) OUTX_L_G);
-    float rot_x = get_angle(twosComplement_to_int(rot_x_high, rot_x_low));
-    int rot_x_int = (int)(rot_x * 100);
-    printf("IMU x angle: %d\n", rot_x_int);
-
-    rot_y_high = read_imu((char) OUTY_H_G);
-    rot_y_low = read_imu((char) OUTY_L_G);
-    float rot_y = get_angle(twosComplement_to_int(rot_y_high, rot_y_low));
-    int rot_y_int = (int)(rot_y * 100);
-    printf("IMU y angle: %d\n", rot_y_int);
-  }*/
-}
 ////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////
